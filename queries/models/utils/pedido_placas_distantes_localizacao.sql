1c62c8332ec00c50abd599d4d898bc2dfe477765
d116626 <d116626@gmail.com>
2024-07-01 20:16:26 -0300

    feat: add new utils query

--- queries/models/utils/pedido_placas_distantes_localizacao.sql
+++ queries/models/utils/pedido_placas_distantes_localizacao.sql
@@ -7,17 +7,18 @@ WITH loc AS (
     CAST(t1.longitude AS FLOAT64) AS longitude,
     ST_GEOGPOINT(CAST(latitude AS FLOAT64), CAST(longitude AS FLOAT64)) AS location,
     ST_DISTANCE(
-      ST_GEOGPOINT(latitude, longitude), -- posicao
+      ST_GEOGPOINT(latitude, longitude), -- target position
       ST_GEOGPOINT(CAST(t1.latitude AS FLOAT64), CAST(t1.longitude AS FLOAT64))
     ) AS distance,
   FROM `rj-cetrio.ocr_radar.equipamento` t1
   JOIN `rj-cetrio.ocr_radar.equipamento_codcet_to_camera_numero` t2
     ON t1.codcet = t2.codcet
   WHERE ST_DWithin(
-    ST_GEOGPOINT(latitude, longitude), -- posicao
+    ST_GEOGPOINT(latitude, longitude), -- target position
     ST_GEOGPOINT(CAST(t1.latitude AS FLOAT64), CAST(t1.longitude AS FLOAT64)),
-    300
+    300 -- target distance (radars less than X meters)
     )
+    AND t1.locequip LIKE "%SENTIDO CENTRO%"
 )
 
 SELECT
@@ -33,5 +34,4 @@ FROM `rj-cetrio.ocr_radar.readings_2024_07` t1
 JOIN loc t2
   ON t1.camera_numero = t2.camera_numero
 WHERE DATETIME(datahora, "America/Sao_Paulo") BETWEEN "2024-07-01T00:00:00" AND "2024-07-01T00:00:00"
-  AND t2.locequip LIKE "%SENTIDO CENTRO%"
 ORDER BY distance, t2.camera_numero, datahora
