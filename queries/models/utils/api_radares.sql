b47abef62d60c6c664bf202f8b22413252c71a18
d116626 <d116626@gmail.com>
2024-07-19 12:36:49 -0300

    chore: add empresa, last_detection_time and active_in_last_24_hours columns

--- queries/models/utils/api_radares.sql
+++ queries/models/utils/api_radares.sql
@@ -15,29 +15,38 @@ WITH radars AS (
 
 used_radars AS (
   SELECT
-    DISTINCT
     camera_numero,
     camera_latitude,
     camera_longitude,
+    empresa,
+    MAX(DATETIME(datahora, "America/Sao_Paulo")) AS last_detection_time,
     'active' AS status
   FROM `rj-cetrio.ocr_radar.readings_*`
+  GROUP BY camera_numero, camera_latitude, camera_longitude, empresa
 ),
 
 selected_radar AS (
   SELECT
     t1.codcet,
     COALESCE(t1.camera_numero, t2.camera_numero) AS camera_numero,
+    COALESCE(t2.empresa, NULL) AS empresa,
     COALESCE(t1.latitude, t2.camera_latitude) AS latitude,
     COALESCE(t1.longitude, t2.camera_longitude) AS longitude,
     t1.locequip,
     t1.bairro,
     t1.logradouro,
     t1.sentido,
-    COALESCE(t2.status, 'inactive') AS status
+    COALESCE(t2.status, 'inactive') AS status,
+    COALESCE(t2.last_detection_time, NULL) AS last_detection_time,
+    CASE
+      WHEN t2.last_detection_time IS NULL THEN NULL
+      WHEN TIMESTAMP(t2.last_detection_time) >= TIMESTAMP_SUB(TIMESTAMP(CURRENT_DATETIME("America/Sao_Paulo")), INTERVAL 24 HOUR) THEN 'yes'
+      ELSE 'no'
+    END AS active_in_last_24_hours
   FROM radars t1
   FULL OUTER JOIN used_radars t2
     ON t1.camera_numero = t2.camera_numero
 )
 
 SELECT * FROM selected_radar
-ORDER BY camera_numero
+ORDER BY last_detection_time DESC
