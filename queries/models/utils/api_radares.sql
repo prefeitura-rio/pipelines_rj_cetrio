4abbe7e2dfca368d854f78fdce33d050c164b03f
d116626 <d116626@gmail.com>
2024-07-19 11:30:47 -0300

    chore: add status column to api_radars query

--- queries/models/utils/api_radares.sql
+++ queries/models/utils/api_radares.sql
@@ -14,27 +14,29 @@ WITH radars AS (
 ),
 
 used_radars AS (
-    SELECT
-        DISTINCT
-        camera_numero,
-        camera_latitude,
-        camera_longitude
-    FROM `rj-cetrio.ocr_radar.readings_*`
+  SELECT
+    DISTINCT
+    camera_numero,
+    camera_latitude,
+    camera_longitude,
+    'active' AS status
+  FROM `rj-cetrio.ocr_radar.readings_*`
 ),
 
 selected_radar AS (
   SELECT
-        t1.codcet,
-        COALESCE(t1.camera_numero, t2.camera_numero) AS camera_numero,
-        COALESCE(t1.latitude, t2.camera_latitude) AS latitude,
-        COALESCE(t1.longitude, t2.camera_longitude) AS longitude,
-        t1.locequip,
-        t1.bairro,
-        t1.logradouro,
-        t1.sentido
-    FROM radars t1
-    FULL OUTER JOIN used_radars t2
-        ON t1.camera_numero = t2.camera_numero
+    t1.codcet,
+    COALESCE(t1.camera_numero, t2.camera_numero) AS camera_numero,
+    COALESCE(t1.latitude, t2.camera_latitude) AS latitude,
+    COALESCE(t1.longitude, t2.camera_longitude) AS longitude,
+    t1.locequip,
+    t1.bairro,
+    t1.logradouro,
+    t1.sentido,
+    COALESCE(t2.status, 'inactive') AS status
+  FROM radars t1
+  FULL OUTER JOIN used_radars t2
+    ON t1.camera_numero = t2.camera_numero
 )
 
 SELECT * FROM selected_radar
