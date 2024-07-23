03497c6f97c501aca7b74fb104be787e5dc310d1
d116626 <d116626@gmail.com>
2024-07-23 17:27:40 -0300

    fix: equipamento model

--- queries/models/ocr_radar/equipamento.sql
+++ queries/models/ocr_radar/equipamento.sql
@@ -5,6 +5,6 @@ SELECT
     CAST(latitude AS FLOAT64) as latitude,
     CAST(longitude AS FLOAT64) as longitude,
     CAST(logradouro AS STRING) as logradouro,
-    CAST(sentido AS STRING) as sentido
+    CAST(sentido AS STRING) as sentido,
     CAST(sentido AS INT64) as velofisc
 FROM `rj-cetrio.ocr_radar_staging.equipamento`
